eb41ed5ce4e514dda92fcc969c089b7c1b051647
Hector Caudel <kaudel@gmail.com>
2026-01-08 04:24:04 -0600

    Added Jumbotron component, pages and routes structure in router

--- src/router/app.router.tsx
+++ src/router/app.router.tsx
@@ -1,9 +1,11 @@
 import AdminLayout from "@/admin/layout/AdminLayout";
 import AdminPage from "@/admin/pages/AdminPage";
-import HomePage from "@/game/home/HomePage";
+import HomePage from "@/game/pages/home/HomePage";
 import GameLayout from "@/game/layouts/GameLayout";
 import { lazy } from "react";
 import { createBrowserRouter, Navigate } from "react-router";
+import FavoritesPage from "@/game/pages/favoritos/FavoritesPage";
+import StatsPage from "@/game/pages/stats/StatsPage";
 
 /*Sera usada en layloading */
 //const AdminPage = lazy(() => import("@/admin/pages/AdminPage"));
@@ -14,6 +16,8 @@ export const appRouter = createBrowserRouter([
     element: <GameLayout />,
     children: [
       { index: true, element: <HomePage /> },
+      { path: "/favorites", element: <FavoritesPage /> },
+      { path: "/stats", element: <StatsPage /> },
       { path: "*", element: <Navigate to="/" /> },
     ],
   },
